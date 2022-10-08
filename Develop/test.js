3a7c81ed52774447a5385dea1b15b563dfd447f2
Jamey Wicklund <theBluWiz@icloud.com>
2022-10-08 14:27:55 -0700

    Adds logic pattern

--- Develop/test.js
+++ Develop/test.js
@@ -1,21 +1,4 @@
-///////////////////////// DO NOT CHANGE ////////////////////////////////////
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  passwordText.value = password;
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
-
-function generatePassword() {
-  /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
-  const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   const numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
   const specialCharacterArray = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
@@ -31,34 +14,30 @@ function generatePassword() {
 
   if (includeLowercase) {
     for (let i = 0; i < lowercaseArray.length; i++) {
-      allowedCharacters.push(lowercaseArray[i]);
+      allowedCharacters.push(lowercaseArray[i])
     }
   }
 
   if (includeUppercase) {
     for (let i = 0; i < uppercaseArray.length; i++) {
-      allowedCharacters.push(uppercaseArray[i]);
+      allowedCharacters.push(uppercaseArray[i])
     }
   }
 
   if (includeNumbers) {
     for (let i = 0; i < numberArray.length; i++) {
-      allowedCharacters.push(numberArray[i]);
+      allowedCharacters.push(numberArray[i])
     }
   }
 
   if (includeSpecialCharacters) {
     for (let i = 0; i < specialCharacterArray.length; i++) {
-      allowedCharacters.push(specialCharacterArray[i]);
+      allowedCharacters.push(specialCharacterArray[i])
     }
   }
   
   for (let i = 0; i < passwordLength; i++) {
-    passwordArray.push(Math.floor(Math.random() * allowedCharacters.length));
+    passwordArray.push(Math.floor(Math.random() * allowedCharacters.length))
   }
 
- password = passwordArray.join();
-console.log(password)
-
- return passwordArray;
-}
+ password = passwordArray.join()
