684b5f52d065b489e0e39bafecff8323c6dd0dde
Jamey Wicklund <theBluWiz@icloud.com>
2022-10-08 14:00:28 -0700

    Adds const arrays and user questions

--- Develop/script.js
+++ Develop/script.js
@@ -15,4 +15,15 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
   /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
+  const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  const numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const specialCharacterArray = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
+
+
+  let passwordLength = prompt("Choose a password length between 8 and 128 characters long.");
+  let includeLowercase = confirm("Should we include lowercase characters?");
+  let includeUppercase = confirm("Should we include uppercase characters?");
+  let includeNumbers = confirm("Should we include numbers?");
+  let includeSpecialCharacters = confirm("Should we include special characters?");
 }
