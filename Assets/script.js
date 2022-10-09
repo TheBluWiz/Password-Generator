fcc9672218e80452c97cadbbca46b58f20ac6e23
Jamey Wicklund <theBluWiz@icloud.com>
2022-10-08 17:00:37 -0700

    Created README.MD and added comments to javaScript

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,3 @@
-///////////////////////// DO NOT CHANGE ////////////////////////////////////
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -11,10 +10,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
 
 function generatePassword() {
-  /////////////////////////// WRITE YOUR CODE HERE /////////////////////////
+  // Creates arrays for each requirement type
   const lowercaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   const uppercaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   const numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -23,15 +21,22 @@ function generatePassword() {
   var passwordArray = [];
   var password;
 
+  //Set password length
   let passwordLength = prompt("Choose a password length between 8 and 128 characters long.");
 
+  // checks if password is within viable length
   if (passwordLength >= 8 && passwordLength <= 128) {
+
+    // checks User password requirements
     let includeLowercase = confirm("Should we include lowercase characters?");
     let includeUppercase = confirm("Should we include uppercase characters?");
     let includeNumbers = confirm("Should we include numbers?");
     let includeSpecialCharacters = confirm("Should we include special characters?");
 
+    // confirms at least one of required data types chosen
     if (includeLowercase || includeUppercase || includeNumbers || includeSpecialCharacters) {
+      
+      // adds user required data types to allowedCharacters
       if (includeLowercase) {
         for (let i = 0; i < lowercaseArray.length; i++) {
           allowedCharacters.push(lowercaseArray[i])
@@ -56,11 +61,13 @@ function generatePassword() {
         }
       }
 
+      // assign random characters to User password array up to specified password length
       for (let i = 0; i < passwordLength; i++) {
         random = Math.floor(Math.random() * allowedCharacters.length);
         passwordArray.push(allowedCharacters[random])
       }
 
+      // transfers array into string for output
       password = passwordArray.join('');
       return password;
     } else {
---
+++ ReadME.md
@@ -0,0 +1,15 @@
+# Password Generator
+## Description
+The goal of this WebAPP is to flexibly generate passwords based off of the requirements of various websites. At launch, this allows a character range of 8 to 128 characters, and allows you to select requirements such as upper and lower case characters, numbers, and special characters. I reccomend selecting as many requiremnets as your website reports and choosing the maximum allowed number of characters to achieve the highest level of security. 
+
+## Tasks
+- Built Arrays for each requirement type
+- Added logic to prompt errors for unexpected User Input
+- Created itterative loops to make an array of available characters based on User requirements
+- Created itterative loop to assign random characters to User password string up to specified password length
+
+## Example
+![webiste render](assets/images/splash-page.png)
+
+## Deployment
+The finished website can be found: 
